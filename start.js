4c771be02bcb696fc99876837ac54dd7a2de2cb3
xudafeng <xudafeng@126.com>
2023-03-02 18:21:03 +0800

    feat: support window state

--- start.js
+++ start.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const { app: electronApp } = require('electron');
-const initElectrom = require('./app/electrom');
 
 const App = require('./app');
 
 electronApp.on('ready', async () => {
   const app = new App();
   app.init();
-  initElectrom();
 });
